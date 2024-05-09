5cdfea09a3bf3e6d80231b57354e20e5a96beb71
psyjs34 <psyjs34@nottingham.ac.uk>
2024-05-09 20:38:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 const supabase = createClient('https://olezxgvjwaapmpvuuxhx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sZXp4Z3Zqd2FhcG1wdnV1eGh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyNjQ0ODEsImV4cCI6MjAzMDg0MDQ4MX0.QhebJQ6x66GSbZ9xK4LHU32R-zLAwdd_KCBYInZlfys');
-
-document.addEventListener('DOMContentLoaded', function() {
-
     async function fetchDataName(inputData) {
         const { data, error } = await supabase
         .from('People')
@@ -128,6 +125,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+document.addEventListener('DOMContentLoaded', function() {
+
     document.getElementById('searchForm').addEventListener('submit', async function(e) {
         e.preventDefault(); // Prevent the default form submission behavior
         
@@ -162,30 +161,32 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
-    // Vehicle Search Listener
-document.getElementById('searchForm2').addEventListener('submit', async function(e) {
-    e.preventDefault(); // Prevent the default form submission behavior
-    
-    const searchResults = document.querySelectorAll('.searchresult');
-
-    // Loop through each element and set its display to 'none'
-    searchResults.forEach(element => {
-        element.style.display = 'none';
-    });
-    // Get input values
-    var regoInput = document.getElementById('rego').value.trim();
-
-    // Get message div
-    var messageDiv = document.getElementById('message2');
-    
-    // Check if both fields are empty
-    if (regoInput === '') { // <-- Fixed condition to use regoInput
-        messageDiv.textContent = 'Error, field empty';
-    }
-    // Search database if only one input
-    else {
-        fetchDataRego(regoInput);
-    }
 });
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('searchForm2').addEventListener('submit', async function(e) {
+        e.preventDefault(); // Prevent the default form submission behavior
+        
+        const searchResults = document.querySelectorAll('.searchresult');
+    
+        // Loop through each element and set its display to 'none'
+        searchResults.forEach(element => {
+            element.style.display = 'none';
+        });
+        // Get input values
+        var regoInput = document.getElementById('rego').value.trim();
+    
+        // Get message div
+        var messageDiv = document.getElementById('message2');
+        
+        // Check if both fields are empty
+        if (regoInput === '') { // <-- Fixed condition to use regoInput
+            messageDiv.textContent = 'Error, field empty';
+        }
+        // Search database if only one input
+        else {
+            fetchDataRego(regoInput);
+        }
+    });
 });
